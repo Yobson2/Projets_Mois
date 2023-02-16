754dfb96d186af3ae29e28e2ad60c331a77addac
yoboue louis <yobouelouiscarlos@gmail.com>
2023-02-16 12:20:38 +0100

    my global commit

---
+++ e-commerce_site/JS/HomeFirst.js
@@ -0,0 +1,15 @@
+
+let miniModal=document.querySelector('.modal-page');
+let checkUser=document.querySelector('.icon-user');
+    //   miniModale
+checkUser.addEventListener('click', ()=>{
+    miniModal.classList.toggle('open');
+})
+
+let checkSearchBarre=document.querySelector('.icon-search');
+let barreSearch=document.querySelector('.header-search-barre');
+
+checkSearchBarre.addEventListener('click', ()=>{
+    barreSearch.classList.toggle('open-barre');
+    console.log('barre')
+})
--- src/composables/saveData.js
+++
@@ -1,8 +0,0 @@
-import { db } from "../../db/Conf";
-import { collection, doc, addDoc } from "firebase/firestore";
-
-
-export const saveOne = async (_collection, _doc)=>{
-    await addDoc(collection(db,_collection), _doc)
-    console.log('added')
-}
