b8026f19729546e427b0a0da8f9541d3dac1d78b
yoboue louis <yobouelouiscarlos@gmail.com>
2023-03-30 14:23:09 +0200

    level 10

--- Agrico_gestion/JS/inscrit.js
+++ Agrico_gestion/JS/inscrit.js
@@ -1,3 +1,4 @@
+// let fichier = document.querySelector('input[type="file"]');
 let fichier = document.querySelector('input[type="file"]');
 let form = document.getElementById("inscription");
 let nom=document.querySelector('#Nom');
