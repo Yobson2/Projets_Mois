60ff9b579691e0f6b41f070b05729dff97688ac5
yoboue louis <yobouelouiscarlos@gmail.com>
2023-06-15 15:56:58 +0200

    insert two forms

--- Transport/transport_test/src/App.jsx
+++ Transport/transport_test/src/App.jsx
@@ -3,14 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Board from './square'
+import Home from './Home'
+import {Routes,Route,Link} from "react-router-dom"
 
 function App() {
   const [count, setCount] = useState(0)
    
   return (
     <> 
-      <Board />
-      
+      {/* <Board /> */}
+       
+     <Link to="/Home">Mon profit</Link>
+      <Routes>
+            <Route path='/Home' element={<Home />}></Route>
+      </Routes>
+
     </>
   )
 }
